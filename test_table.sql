2451774101d73cbe088250dda3b1c79a98289921
Pan, Wen-Ming <smalljelly2018@gmail.com>
2020-11-17 12:03:24 +0800

    Update test_table.sql

--- test_table.sql
+++ test_table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `test` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
-  `timestamp` varchar(30) DEFAULT NULL COMMENT '时间戳',
-  `msg` varchar(255) DEFAULT NULL COMMENT '信息',
-  `other` varchar(255) DEFAULT NULL COMMENT '随机字符',
-  PRIMARY KEY (`id`)
+CREATE TABLE test(
+  id int(12) NOT NULL AUTO_INCREMENT,
+  timestamp varchar(50) DEFAULT NULL,
+  msg varchar(255) DEFAULT NULL,
+  other varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=33811 DEFAULT CHARSET=utf8mb4;
