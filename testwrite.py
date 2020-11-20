c0aa463b2432c1db12f605b52663d591ab4eb29b
Pan, Wen-Ming <smalljelly2018@gmail.com>
2020-11-20 14:00:22 +0800

    Update testwrite.py

--- testwrite.py
+++ testwrite.py
@@ -15,8 +15,8 @@ def get_random_str(str_length):
 
 class RunSqlToMysql(object):
     def __init__(self):
-        #self.mysql_host = 'pproxy.proxy-cdagscjv6mu0.ap-southeast-1.rds.amazonaws.com'
-        self.mysql_host = 'aurora.cluster-cdagscjv6mu0.ap-southeast-1.rds.amazonaws.com'
+        self.mysql_host = 'pproxy.proxy-cdagscjv6mu0.ap-southeast-1.rds.amazonaws.com'
+        #self.mysql_host = 'aurora.cluster-cdagscjv6mu0.ap-southeast-1.rds.amazonaws.com'
         #self.mysql_host = 'pm1.cdagscjv6mu0.ap-southeast-1.rds.amazonaws.com'
         self.mysql_user = 'admin'
         self.mysql_password = 'Pjy#0618'
@@ -30,33 +30,19 @@ class RunSqlToMysql(object):
             database=self.mysql_db_name,
             port=3306,
             charset='utf8mb4',
-            connect_timeout=1
+            connect_timeout=1,
+            read_timeout=1,
+            write_timeout=1
         )
         return con_engine
 
     @staticmethod
     def run_sql(db_connect, sql):
-        # 使用cursor()方法获取游标
         cursor = db_connect.cursor()
-        # 执行SQL语句
         cursor.execute(sql)
         result = cursor.fetchone()
-        db_connect.commit()  # 提交修改，类似github的commit操作
-        # print(' sql "%s" successfully' % sql)
+        db_connect.commit()
         return result
-        
-            
-    # def run_sql(db_connect, sql):
-    #     # 使用cursor()方法获取游标
-    #     cursor = db_connect.cursor()
-    #     try:
-    #         # 执行SQL语句
-    #         cursor.execute(sql)
-    #         db_connect.commit()  # 提交修改，类似github的commit操作
-    #         print('Insert sql "%s" successfully' % sql)
-    #     except:
-    #         db_connect.rollback()  # 不成功则回滚
-    #         print('Insert sql "%s" ERROR !!!' % sql)
             
     @staticmethod
     def get_sql(table_name,server_id):
@@ -71,10 +57,10 @@ class RunSqlToMysql(object):
                 time.sleep(0.1)
                 mysql_con = self.connect_db()
                 serverid = self.run_sql(mysql_con, "show variables like'server_id';")
+                print(datetime.datetime.now(),':'.join(serverid))
                 insert_sql = self.get_sql('test',':'.join(serverid))
                 self.run_sql(mysql_con, insert_sql)
                 mysql_con.close()
-                print(datetime.datetime.now(),':'.join(serverid))
             except:
                 print('connect fail')
             continue
